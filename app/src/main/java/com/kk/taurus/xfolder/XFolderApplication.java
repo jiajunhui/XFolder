1c858f9e226b3e17583e803faf191f8088476c8c
jiajunhui <309812983@qq.com>
2017-06-06 11:33:16 +0800

    modify some

--- app/src/main/java/com/kk/taurus/xfolder/XFolderApplication.java
+++ app/src/main/java/com/kk/taurus/xfolder/XFolderApplication.java
@@ -1,6 +1,11 @@
 package com.kk.taurus.xfolder;
 
+import android.os.Environment;
+
+import com.bumptech.glide.request.target.ViewTarget;
 import com.kk.taurus.baseframe.FrameApplication;
+import com.xapp.jjh.logtools.config.XLogConfig;
+import com.xapp.jjh.logtools.tools.XLog;
 
 /**
  * Created by Taurus on 2017/5/18.
@@ -10,5 +15,7 @@ public class XFolderApplication extends FrameApplication {
     @Override
     public void onCreateInAppMainProcess() {
         super.onCreateInAppMainProcess();
+        XLog.init(getApplicationContext(),new XLogConfig().setLogDir(Environment.getExternalStorageDirectory()));
+        ViewTarget.setTagId(R.id.glide_tag);
     }
 }
